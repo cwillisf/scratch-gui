068d8072396e9e0f0ed921daf372cc9a8999bf2e
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2023-08-10 07:48:15 -0700

    test(WIP): fixing test/unit/util/sb-file-uploader-hoc.test.jsx

--- test/helpers/intl-helpers.jsx
+++ test/helpers/intl-helpers.jsx
@@ -16,20 +16,23 @@ const defaultIntlConfig = Object.freeze({
     textComponent: 'span'
 });
 
-const shallowWithIntl = (node, {context} = {}) =>
-    shallow(
-        node,
+const shallowWithIntl = (node, {context} = {}) => {
+    //const intl = createIntl(defaultIntlConfig);
+    return shallow(
+        node, // React.cloneElement(node, {intl}),
         {
-            context: Object.assign({}, context),
+            context: Object.assign({}, context), //, {intl})
             wrappingComponent: IntlProvider,
             wrappingComponentProps: defaultIntlConfig
         }
     ).dive();
+    // Why is dive() needed here? Is this due to a change (bug?) in the unofficial enzyme adapter?
+};
 
 const mountWithIntl = (node, {context, childContextTypes} = {}) => {
     const intl = createIntl(defaultIntlConfig);
     return mount(
-        node,
+        node, // React.cloneElement(node, {intl}),
         {
             context: Object.assign({}, context, {intl}),
             childContextTypes: Object.assign({}, {intl: intlShape}, childContextTypes),
